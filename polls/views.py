0b0c1c9f30efa15bf1926baf0058fae8c33aab2b
Joel Stanner <joel@joelstanner.com>
2016-04-05 12:50:12 -0700

    Use get_object_or_404 now

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 """Polls application for django tutorial."""
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -16,10 +16,7 @@ def index(request):
 
 def detail(request, question_id):
     """Return a detail view based on question_id."""
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
