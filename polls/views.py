3343e6123d7d7ea3cec337faca3536992411a9bd
Joel Stanner <joel@joelstanner.com>
2016-04-05 12:18:01 -0700

    Index view uses template now

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 """Polls application for django tutorial."""
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 
@@ -7,8 +8,11 @@ from .models import Question
 def index(request):
     """Index page."""
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(HttpResponse(template.render(context, request)))
 
 
 def detail(request, question_id):
