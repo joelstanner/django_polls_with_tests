e8b95edaf5aad03ee95a58d5e9dc58dfe8e72ca0
Joel Stanner <joel@joelstanner.com>
2016-04-05 12:24:23 -0700

    Use render shortcut

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 """Polls application for django tutorial."""
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Question
 
@@ -8,11 +8,10 @@ from .models import Question
 def index(request):
     """Index page."""
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(HttpResponse(template.render(context, request)))
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
