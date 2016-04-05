af68319e65d156390b153a7d6a48116509c5e6fd
Joel Stanner <joel@joelstanner.com>
2016-04-04 23:13:16 -0700

    Add models, urls, and views - initial
    This is up through the first part of part 3.

---
+++ polls/test_tests.py
@@ -0,0 +1,53 @@
+"""Tests for django polls tutorial app."""
+from django.test import TestCase
+from django.test import Client
+from django.utils import timezone
+import datetime
+
+
+class PollsViewsTest(TestCase):
+    """Define the various view tests for the polls app."""
+
+    def test_index_view_correct(self):
+        c = Client()
+        response = c.get('/polls/')
+        assert response.content.decode() == "Hello, world. You're at the polls index."  # NOQA
+
+    def test_detail_view(self):
+        c = Client()
+        response = c.get('/polls/1/')
+        assert response.content.decode() == "You're looking at question 1."
+
+    def test_results_view(self):
+        c = Client()
+        response = c.get('/polls/1/results/')
+        expected = "You're looking at the results of question 1."
+        assert response.content.decode() == expected
+
+    def test_vote_view(self):
+        c = Client()
+        response = c.get('/polls/1/vote/')
+        assert response.content.decode() == "You're voting on question 1."
+
+
+class PollsModelTest(TestCase):
+
+    def test_was_published_recently(self):
+        from polls.models import Question
+        q = Question(question_text="test 1", pub_date=timezone.now())
+        q2 = Question(question_text="test 2",
+                      pub_date=timezone.now() - datetime.timedelta(days=2))
+
+        assert q.was_published_recently() is True
+        assert q2.was_published_recently() is False
+
+    def test_str_of_question(self):
+        from polls.models import Question
+        q = Question(question_text="test 1", pub_date=timezone.now())
+        resp = str(q)
+        assert resp == "test 1"
+
+    def test_str_of_choice(self):
+        from polls.models import Choice
+        c = Choice(choice_text="choice 1")
+        assert str(c) == "choice 1"
---
+++ polls/views.py
@@ -0,0 +1,23 @@
+"""Polls application for django tutorial."""
+from django.http import HttpResponse
+
+
+def index(request):
+    """Index page."""
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, question_id):
+    """Return a detail view based on question_id."""
+    return HttpResponse("You're looking at question {}".format(question_id))
+
+
+def results(request, question_id):
+    """Return a results view based on question_id."""
+    response = "You're looking at the results of question {}"
+    return HttpResponse(response.format(question_id))
+
+
+def vote(request, question_id):
+    """Return a vote view based on question_id."""
+    return HttpResponse("You're voting on question {}.".format(question_id))
