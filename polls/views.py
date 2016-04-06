e1c79be83ceaea6e1098604618ade21ed990a8cc
Joel Stanner <joel@joelstanner.com>
2016-04-06 13:12:32 -0700

    Update views and urls to use class based views

--- polls/views.py
+++ polls/views.py
@@ -2,29 +2,34 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
+from django.views.generic import ListView, DetailView
 
 from .models import Question, Choice
 
 
-def index(request):
-    """Index page."""
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(ListView):
+    """Main index view."""
+
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    """Return a detail view based on question_id."""
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(DetailView):
+    """Main Detail View."""
+
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    """Return a results view based on question_id."""
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(DetailView):
+    """Main Results View."""
+
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
